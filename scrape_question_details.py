672ac3bd3c62e4d4c9cb60ad951ca5b36a6499f2
Aflah <72096386+aflah02@users.noreply.github.com>
2022-08-24 22:53:35 +0530

    Removed Pandas

--- scrape_question_details.py
+++ scrape_question_details.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import csv
 
 with open('cross_validated.txt', 'r') as f:
     links = f.readlines()
@@ -9,14 +9,21 @@ BASE_URL = "https://stats.stackexchange.com/"
 
 list_results = []
 
-for sublink in links:
-    link = BASE_URL + sublink.strip()
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.find('a', class_='question-hyperlink').text
-    question_body = " ".join(soup.find('div', class_='s-prose js-post-body').text.replace('\n', ' ').replace('\t', ' ').strip().split())
-    list_results.append([link, title, question_body])
+with open('cross_validated_details.csv', 'w', encoding='utf-8') as csvfile:
+    fieldnames = ['link', 'title', 'question_body']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for sublink in links:
+        try:
+            link = BASE_URL + sublink.strip()
+            page = requests.get(link)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            title = soup.find('a', class_='question-hyperlink').text
+            question_body = " ".join(soup.find('div', class_='s-prose js-post-body').text.replace('\n', ' ').replace('\t', ' ').strip().split())
+            list_results.append([link, title, question_body])
+            print("At Link: " + link + " Title: " + title + " Question Body: " + question_body)
+            writer.writerow({'link': link, 'title': title, 'question_body': question_body})
+        except:
+            print(f"{link} failed")
+            continue
 
-df = pd.DataFrame(list_results)
-df.columns = ['link', 'title', 'question_body']
-df.to_csv('cross_validated_details.csv', index=False)
